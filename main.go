6301f49fdb21738b6d4ef242728217e38e159fd8
Ivan Orlov <camarada.0re1@gmail.com>
2024-09-12 11:58:46 -0700

    Playable

--- main.go
+++ main.go
@@ -5,16 +5,21 @@ import (
 	"fmt"
 	"image/color"
 	"log"
+	"slices"
 	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
 
 var (
 	mplusFaceSource *text.GoTextFaceSource
+	debugColor	  color.Color
+	colorWhite	  color.Color
 )
 
 func init() {
@@ -23,28 +28,47 @@ func init() {
 		log.Fatal(err)
 	}
 	mplusFaceSource = s
+	debugColor = color.RGBA{0xff, 0x80, 0x0D, 0}
+	colorWhite = color.RGBA{0xfa, 0xfa, 0xfa, 0xff}
 }
 
 const (
 	screenWidth  = 640
 	screenHeight = 480
-)
-
-const (
-	tileSize = 30
+	tileSize = 40
+	debug = false
+	
 )
 
 type Board struct {
 	W, H int
 	b    []int
+	m    []bool
 }
 
-func (b *Board) At(x int, y int) int {
-	return b.b[y*b.W+x]
+func (b *Board) At(x int, y int) (int, bool) {
+	return b.b[y*b.W+x], b.m[y*b.W+x]
 }
 
-func (b *Board) Set(x int, y int, v int) {
-	b.b[y*b.W+x] = v
+func (b *Board) Mark(x int, y int, v bool) {
+	b.m[y*b.W+x] = v
+}
+
+func drawRect(screen *ebiten.Image, ax float32, ay float32, bx float32, by float32, thickness float32, clr color.Color) {
+	antialias := false
+	vector.StrokeLine(screen, ax, ay, ax, by, thickness, clr, antialias)
+	vector.StrokeLine(screen, ax, by, bx, by, thickness, clr, antialias)
+	vector.StrokeLine(screen, bx, by, bx, ay, thickness, clr, antialias)
+	vector.StrokeLine(screen, bx, ay, ax, ay, thickness, clr, antialias)		
+}
+
+func (b *Board) centerPos(x, y int) (float32, float32) {
+	ulx := (screenWidth - b.W*tileSize) / 2
+	uly := (screenHeight - b.H*tileSize) / 2
+
+	cx := float32(ulx+x*tileSize) + 0.5 * tileSize
+	cy := float32(uly+y*tileSize) + 0.5 * tileSize
+	return cx, cy
 }
 
 func (b *Board) Draw(screen *ebiten.Image) {
@@ -53,21 +77,32 @@ func (b *Board) Draw(screen *ebiten.Image) {
 
 	for x := 0; x < b.W; x++ {
 		for y := 0; y < b.H; y++ {
-			clr := color.RGBA{0xfa, 0xfa, 0xfa, 0}
+			
+			n, m := b.At(x, y)
+			cx, cy := float32(ulx+x*tileSize), float32(uly+y*tileSize)
 
+			clr := colorWhite
+			if m {
+				clr = 	color.RGBA{0x2a, 0x2a, 0x2a, 0xff}
+			}
+			
 			op := &text.DrawOptions{}
-			op.GeoM.Translate(float64(ulx+x*tileSize), float64(uly+y*tileSize))
+			op.GeoM.Translate(
+				float64(cx + 0.2 * tileSize), 
+				float64(cy - 0.23 * tileSize))
 			op.ColorScale.ScaleWithColor(clr)
 
-			text.Draw(screen, strconv.Itoa(b.At(x, y)), &text.GoTextFace{
+			
+			text.Draw(screen, strconv.Itoa(n), &text.GoTextFace{
 				Source: mplusFaceSource,
 				Size:   tileSize,
 			}, op)
 
-			// if b.At(x, y) {
-			// 	clr = color.RGBA{0xFA, 0xFA, 0xFA, 0}
-			// }
-			// vector.DrawFilledRect(screen, float32(ulx+x*tileSize), float32(uly+y*tileSize), tileSize, tileSize, clr, false)
+			if debug {
+				drawRect(screen, cx, cy, cx+tileSize, cy+tileSize, 1, debugColor)
+				px, py := b.centerPos(x, y)
+				drawRect(screen, px-1, py-1, px+1, py+1, 1, debugColor)
+			}
 		}
 	}
 }
@@ -84,28 +119,76 @@ func (b *Board) ClickPos(x, y int) (bool, int, int) {
 
 type Game struct {
 	hist []Board
+	mx, my float32 // mouse position
 }
 
 func (g *Game) b() *Board {
 	return &g.hist[len(g.hist)-1]
 }
 
+func (g *Game) doSel(cx, cy float32) {
+	lx, rx, ly, ry := min(g.mx, cx), max(g.mx, cx), min(g.my, cy), max(g.my, cy)
+
+	b := g.b()
+	total := 0
+	sx, sy := []int{}, []int{}
+
+	for x := 0; x < b.W; x++ {
+		for y := 0; y < b.H; y++ {
+			n, m := b.At(x, y)
+			if m {
+				continue
+			}
+			nx, ny := b.centerPos(x, y)
+			if nx >= lx && nx <= rx && ny >= ly && ny <= ry {
+				total += n
+				sx, sy = append(sx, x), append(sy, y)
+			}
+		}
+	}
+
+	if total == 0 || total % 10 != 0 {
+		return
+	}
+
+	nb := *b
+	nb.b = slices.Clone(b.b)
+	nb.m = slices.Clone(b.m)
+	for i := 0; i < len(sx); i++ {
+		nb.Mark(sx[i], sy[i], true)
+	}
+
+	g.hist = append(g.hist, nb)
+}
+
 func (g *Game) Update() error {
-	// if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-	// 	mx, my := ebiten.CursorPosition()
-	// 	if ok, cx, cy := g.b().ClickPos(mx, my); ok {
-	// 		g.hist = append(g.hist, g.b().Fill(cx, cy))
-	// 	}
-	// }
-	// if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) && len(g.hist) > 1 {
-	// 	g.hist = g.hist[:len(g.hist)-1]
-	// }
+	if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+		icx, icy := ebiten.CursorPosition()
+		cx, cy := float32(icx), float32(icy)
+		g.doSel(cx, cy)
+	}
+
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		cx, cy := ebiten.CursorPosition()
+		g.mx, g.my = float32(cx), float32(cy)	
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) && len(g.hist) > 1 {
+		g.hist = g.hist[:len(g.hist)-1]
+	}
+		
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.b().Draw(screen)
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("Steps: %d\n Press  ARROW LEFT to UNDO", len(g.hist)-1))
+
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		cx, cy := ebiten.CursorPosition()
+		bx, by := float32(cx), float32(cy)
+		drawRect(screen, g.mx, g.my, bx, by, 1, colorWhite)		
+	}
+	ebitenutil.DebugPrint(screen, "Press  ARROW LEFT to UNDO")
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -116,7 +199,12 @@ func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Hello there")
 
-	board := Board{W: 5, H: 5, b: []int{4, 6, 7, 8, 1, 3, 8, 4, 1, 9, 5, 7, 2, 1, 8, 3, 5, 5, 9, 2, 9, 5, 8, 2, 8}}
+	board := Board{
+		W: 5, 
+		H: 5, 
+		b: []int{4, 6, 7, 8, 1, 3, 8, 4, 1, 9, 5, 7, 2, 1, 8, 3, 5, 5, 9, 2, 9, 5, 8, 2, 8},
+		m: make([]bool, 5*5),
+	}
 	g := Game{
 		hist: []Board{board},
 	}
